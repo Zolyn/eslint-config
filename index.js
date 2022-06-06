03510e47c972f7acdc33085b8b11a85ce5728610
Zolyn <zorin1477@gmail.com>
2022-06-06 21:05:30 +0800

    chore: update

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 module.exports = {
-    extends: ['./vue.js']
+    extends: ['alloy-patched', 'plugin:import/recommended', 'plugin:import/typescript', 'plugin:prettier/recommended'],
+    rules: {
+        'import/no-unresolved': 'error'
+    },
+    settings: {
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx']
+        },
+        'import/resolver': {
+            typescript: {
+                alwaysTryTypes: true,
+            }
+        }
+    }
 }
