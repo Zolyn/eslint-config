dd5382a05bb3d94d17bacde74904281737ae3aa3
Zolyn <zorin1477@gmail.com>
2022-08-05 05:21:05 +0800

    fix: missing property `parser`

--- ts/vue.js
+++ ts/vue.js
@@ -1,12 +1,14 @@
 module.exports = {
     extends: ['alloy', 'alloy/typescript', 'alloy/vue', './shared.js'],
     parserOptions: {
-        // JS
-        js: '@babel/eslint-parser',
-        jsx: '@babel/eslint-parser',
-        // TS
-        ts: '@typescript-eslint/parser',
-        tsx: '@typescript-eslint/parser',
+        parser: {
+            // JS
+            js: '@babel/eslint-parser',
+            jsx: '@babel/eslint-parser',
+            // TS
+            ts: '@typescript-eslint/parser',
+            tsx: '@typescript-eslint/parser',
+        },
     },
     rules: {
         '@typescript-eslint/prefer-optional-chain': 'off',
